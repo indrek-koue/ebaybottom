51dfa478c055b530dcd2b3ac581d227d86481b42
Indrek Koue <indrek.koue@gmail.com>
2013-03-17 20:18:18 +0200

    trunk

--- src/com/titanium/ebaybottom/Main.java
+++ src/com/titanium/ebaybottom/Main.java
@@ -20,18 +20,16 @@ import org.apache.http.util.EntityUtils;
 import com.google.gson.Gson;
 import com.titanium.ebaybottom.model.EbaySearchResult;
 import com.titanium.ebaybottom.model.SearchResult;
+import com.titanium.ebaybottom.util.ApiKey;
 
 public class Main {
 
-	// private static final String SEARCH_KEYWORD = "ipod";
-	// private static final String API_CALL = "FindItems";
-	// private static final String API_VER = "517";
-	// private static final String EBAY_API_PATH =
-	// "http://open.api.ebay.com/shopping?";
-	// private static final String API_KEY =
-	// "TomThoma-61de-4a88-81bc-38753a959533";
-
+	private static final String RESULT_COUNT = "2";
+	private static final String LOCALE = "EBAY-US";
+	private static final String SEARCH_KEYWORD = "ipod";
 	private static final boolean DEBUG = true;
+
+	static List<Integer> Categories = new ArrayList<>();
 
 	public static void main(String[] args) {
 		try {
@@ -60,25 +58,113 @@ public class Main {
 	}
 
 	private static String buildUri() throws URISyntaxException {
+
+		// http://svcs.ebay.com/services/search/FindingService/v1?
+		// OPERATION-NAME=findItemsAdvanced&
+		// SERVICE-VERSION=1.0.0&
+		// SECURITY-APPNAME=YourAppID&
+		// RESPONSE-DATA-FORMAT=XML&
+		// REST-PAYLOAD&
+		// outputSelector=AspectHistogram&
+		// itemFilter(0).name=Condition&
+		// itemFilter(0).value=Used&
+		// itemFilter(1).name=ListingType&
+		// itemFilter(1).value=AuctionWithBIN&
+		// paginationInput.entriesPerPage=2&
+		// categoryId=31388
+
 		URIBuilder uBuilder = new URIBuilder(
 				"http://svcs.ebay.com/services/search/FindingService/v1");
 
 		// compulsory
 		uBuilder.setParameter("OPERATION-NAME", "findItemsAdvanced");
-		uBuilder.setParameter("SECURITY-APPNAME",
-				"TomThoma-61de-4a88-81bc-38753a959533");
+		uBuilder.setParameter("SECURITY-APPNAME", ApiKey.KEY);
 		uBuilder.setParameter("RESPONSE-DATA-FORMAT", "JSON");
 
 		// optional
-		uBuilder.setParameter("keywords", "ipod");
-		uBuilder.setParameter("GLOBAL-ID", "EBAY-US");
+		uBuilder.setParameter("keywords", SEARCH_KEYWORD);
+		uBuilder.setParameter("GLOBAL-ID", LOCALE);
 		uBuilder.setParameter("outputSelector", "SellerInfo");
-		uBuilder.setParameter("paginationInput.entriesPerPage", "2");
+		uBuilder.setParameter("paginationInput.entriesPerPage", RESULT_COUNT);
+		uBuilder.setParameter("SERVICE-VERSION", "1.12.0");
+
+		// Specifies the category from which you want to retrieve item listings.
+		// This field can be repeated to include multiple categories. Up to
+		// three (3) categories can be specified.
+		//
+		// If a specified category ID doesn't match an existing category for the
+		// site, // eBay returns an invalid-category error message. To determine
+		// valid categories, use the Shopping API GetCategoryInfo call.
+
+		// http://developer.researchadvanced.com/tools/categoryBrowser.php
+
+		// Consumer Electronics;
+		Categories.add(293);
+		// Vehicle Electronics & GPS;
+		Categories.add(3270);
+		// GPS Units
+		Categories.add(156955);
+
+		for (int categoryId : Categories) {
+			uBuilder.setParameter("categoryId", categoryId + "");
+		}
+
+		// 1.1.4 Condition: New
+		// 1.1.5 New other (see details);
+		// 1.1.6 Manufacturer refurbished;
+		// 1.1.7 Seller refurbished;
+		// 1.1.8 Used;
+
+		// outputSelector=AspectHistogram&
+		// itemFilter(0).name=Condition&
+		// itemFilter(0).value=Used&
+		// itemFilter(1).name=ListingType&
+		// itemFilter(1).value=AuctionWithBIN&
+		
+		//http://developer.ebay.com/devzone/finding/callref/types/ItemFilterType.html
+		
+//		1000
+//		New
+//		1500
+//		New other (see details)
+//		1750
+//		New with defects
+//		2000
+//		Manufacturer refurbished
+//		2500
+//		Seller refurbished
+//		3000
+//		Used
+//		4000
+//		Very Good
+//		5000
+//		Good
+//		6000
+//		Acceptable
+//		7000
+//		For parts or not working
+		
+		uBuilder.setParameter("itemFilter(0).name", "Condition");
+		uBuilder.setParameter("itemFilter(0).value(0)", "1000");
+		uBuilder.setParameter("itemFilter(0).value(1)", "1500");
+		uBuilder.setParameter("itemFilter(0).value(2)", "2000");
+		uBuilder.setParameter("itemFilter(0).value(3)", "2500");
+		uBuilder.setParameter("itemFilter(0).value(4)", "3000");
+		// price
+		// ...&itemFilter.name=FreeShippingOnly&itemFilter.value=true...
+		// Some filters take additional parameters. For example, when
+		// using the MaxPrice or MinPrice filters, you can specify the
+		// currency value with itemFilter.paramName and itemFilter.paramValue.
+		// When you use MaxPrice and MinPrice item filters, itemFilter.paramName
+		// defaults to Currency and itemFilter.paramValue defaults to USD.
+		uBuilder.setParameter("itemFilter(3).name", "Condition");
+		uBuilder.setParameter("itemFilter(3).value", "...");
+		uBuilder.setParameter("itemFilter(3).name", "Condition");
+		uBuilder.setParameter("itemFilter(3).value", "...");
+
 		
 		String uri = uBuilder.build().toString();
-
 		printDebug("REQUEST URL:" + uri);
-
 		return uri;
 	}
 
